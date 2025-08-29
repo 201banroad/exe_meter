bdac04d7d64d2f77b447ddfe5ddcde1b13060c01
201banroad <hiroto01021202@icloud.com>
2025-08-29 11:49:55 +0900

    live_secindsまでの定義をした

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,21 @@
 class Session < ApplicationRecord
+
+    def running?   #セッションが進行中かどうか
+        started_at.present? && ended_at.nil?
+    end
+
+    def persisted_seconds #すでにDBに保存されている合計秒数。total_seconds がnilのときは 0 を返す（エラー防止）
+        total_seconds || 0
+    end
+
+    def live_seconds #タイマー走っている場合と止まってる場合での経過時間
+        if running?
+            persisted_seconds + ( Time.current - started_at ).to_i
+        else
+            persisted_seconds
+        end
+    end
+
+
+
 end
