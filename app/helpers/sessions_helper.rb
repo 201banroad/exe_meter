ca3b67dad35f453bf1188e96f8c4a551e7fada96
201banroad <hiroto01021202@icloud.com>
2025-09-26 22:23:54 +0900

    ビューに今日のサマリー追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+    
+  def format_duration(seconds)
+    s = seconds.to_i
+    h = s / 3600
+    m = (s % 3600) / 60
+    sec = s % 60
+    format("%02d:%02d:%02d", h, m, sec)
+  end
+
+
+
+
+
+
+
+
 end
+
+
