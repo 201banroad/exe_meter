7a09360f41ecb9db4d859fcfe5077fda84d068ff
201banroad <hiroto01021202@icloud.com>
2025-12-29 23:21:50 +0900

    configのEC2との差分をなくす

--- config/puma.rb
+++ config/puma.rb
@@ -24,18 +24,34 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# スレッド数
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+min_threads_count = max_threads_count
+threads min_threads_count, max_threads_count
+
+# ポート
+port ENV.fetch("PORT", 3000)
+
+# 環境
+environment ENV.fetch("RAILS_ENV", "production")
+
+# PID
+pidfile ENV.fetch("PIDFILE", "tmp/pids/puma.pid")
+
+# ログ
+stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
+
+
+# 再起動対応
+plugin :tmp_restart
